8f034684d5a3959b2f75df43769633ab7532ee99
Ghada Chebbi <ghada.chebbi1@esprit.tn>
2022-10-17 21:57:09 +0100

    first try

---
+++ src/main/java/tn/esprit/rh/achat/entities/Operateur.java
@@ -0,0 +1,45 @@
+package tn.esprit.rh.achat.entities;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Operateur implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idOperateur;
+	private String nom;
+	private String prenom;
+	
+	private String password;
+	@OneToMany
+	@JsonIgnore
+	private Set<Facture> factures;
+
+	public Operateur(Long idOperateur, String nom, String prenom, String password) {
+		super();
+		this.idOperateur = idOperateur;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.password = password;
+	}
+}
